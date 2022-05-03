0840ab0203ada95130afb42438c7042724365ce5
FRanggaY <franciscusrangga@gmail.com>
2022-05-03 20:31:34 +0700

    create middleware ceklevel and islogin

---
+++ app/Http/Middleware/Is_login.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Is_Login
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user() == false) {
+            return $next($request);
+        }else{
+            return redirect('/dashboard');
+        }
+        if(Auth::user()->status == 0){
+            return redirect('login')->with('error',"this account is not active. Please contact admin!");
+        }else{
+            return redirect('/dashboard');
+        }
+    }
+}
