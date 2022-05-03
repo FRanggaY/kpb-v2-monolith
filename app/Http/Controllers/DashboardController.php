0b1e80cb0ef1bcfa589826bb3f182b184d6f6fd8
FRanggaY <franciscusrangga@gmail.com>
2022-05-03 20:40:13 +0700

    dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\gallery;
+use App\Models\User;
+use App\Models\advertise;
+use App\Models\activity;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        // user
+        $user_total_admin = User::where('role', 'admin')->count();
+        $user_total_user = User::where('role', 'user')->count();
+        $user_total = $user_total_admin + $user_total_user;
+
+        // gallery
+        $gallery_total = gallery::count();
+        $gallery_active = gallery::where('status', 1)->count();
+        $gallery_unactive = gallery::where('status', 0)->count();
+
+        // advertise
+        $advertise_total = advertise::count();
+        $advertise_active = advertise::where('status', 1)->count();
+        $advertise_unactive = advertise::where('status', 0)->count();
+
+        //activity
+        $activity_total = activity::count();
+        $activity_today = activity::whereDate('date', now())->limit(4)->get();
+
+        $datas = [
+            'user_total' => $user_total,
+            'user_total_admin' => $user_total_admin,
+            'user_total_user' => $user_total_user,
+            'gallery_total' => $gallery_total,
+            'gallery_active' => $gallery_active,
+            'gallery_unactive' => $gallery_unactive,
+            'advertise_total' => $advertise_total,
+            'advertise_active' => $advertise_active,
+            'advertise_unactive' => $advertise_unactive,
+            'activity_total' => $activity_total,
+            'activity_today' => $activity_today,
+        ];
+
+
+        return view('admin.dashboard.index', ['datas' => $datas]);
+    }
+}
---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.main_dashboard')
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+      <h1>Dashboard</h1>
+    </div>
+
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                  <div class="card-stats">
+                    <div class="card-stats-title">User Statistics
+                    </div>
+                    <div class="card-stats-items">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['user_total_admin'] }}</div>
+                        <div class="card-stats-item-label">Admin</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['user_total_user'] }}</div>
+                        <div class="card-stats-item-label">User</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="far fa-user"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Users</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $datas['user_total'] }}
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                  <div class="card-stats">
+                    <div class="card-stats-title">Gallery Statistics
+                    </div>
+                    <div class="card-stats-items">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['gallery_active'] }}</div>
+                        <div class="card-stats-item-label">Active</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['gallery_unactive'] }}</div>
+                        <div class="card-stats-item-label">Unactive</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-image"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Gallery</h4>
+                    </div>
+                    <div class="card-body">
+                      {{ $datas['gallery_total'] }}
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                  <div class="card-stats">
+                    <div class="card-stats-title">Advertise Statistics
+                    </div>
+                    <div class="card-stats-items">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['advertise_active'] }}</div>
+                        <div class="card-stats-item-label">Active</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">{{ $datas['advertise_unactive'] }}</div>
+                        <div class="card-stats-item-label">Unactive</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-box"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Advertise</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $datas['advertise_total'] }}
+                    </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-12 col-12 col-sm-12">
+                <div class="card">
+                    <div class="card card-statistic-2 m-0">
+                        <div class="card-icon shadow-primary bg-primary">
+                            <i class="far fa-calendar"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                            <h4>Activity</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $datas['activity_total'] }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-header">
+                        <h4>Activities Today</h4>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-unstyled list-unstyled-border">
+                            @foreach ($datas['activity_today'] as $item)
+                                <li class="media">
+                                    <div class="media-body">
+                                        @if($item->image)
+                                        <img class="mr-3" width="200" src="{{ asset($item->image) }}" alt="photo">
+                                        @endif
+                                        <div class="float-right text-primary">{{ $item->time }}</div>
+                                        <div class="media-title">{{ $item->title }}</div>
+                                        <span class="text-small text-muted">{{ date('j F Y', strtotime($item->date)) }}</span>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                        @if(count($datas['activity_today'])<=0)
+                        <div class="text-center pt-1 pb-1">
+                            <span class="text-muted">No Activities Today</span>
+                        </div>
+                        @else
+                        <div class="text-center pt-1 pb-1">
+                            <a href="{{ url('dashboard/activity') }}" class="btn btn-primary btn-lg btn-round">
+                                View All
+                            </a>
+                        </div>
+                        @endif
+
+                    </div>
+                </div>
+              </div>
+        </div>
+
+        </div>
+    </div>
+  </section>
+@endsection
